710e7653ca07bc9c0706697a312366eb8ab78046
dfhuynh <dfhuynh@36899aa3-f54f-0410-b718-5f787d48e281>
2008-06-21 01:07:13 +0000

    Factored UI stuff from within WindowSession out to PageSession. More refactoring is on the way.

--- trunk/src/extension/content/scripts/window-session.js
+++ trunk/src/extension/content/scripts/window-session.js
@@ -1,15 +1,17 @@
 Companion.WindowSession = function(browser) {
     this.browser = browser;
     
-    this._windowSessionPage = null;
+    this._tabHasFocus = false;
     this._dom = {};
+    this._windowSessionPage = null;
     
     this._uiInitialized = false;
     this._keyToPageSession = {};
+    this._currentPageSession = null;
 };
 
 Companion.WindowSession.prototype.getCurrentPageSessionKey = function() {
-    return this._browser.contentWindow.location.href;
+    return this.browser.contentWindow.location.href;
 };
 
 Companion.WindowSession.prototype.getCurrentPageSession = function(create) {
@@ -17,7 +19,7 @@ Companion.WindowSession.prototype.getCurrentPageSession = function(create) {
     if (key in this._keyToPageSession) {
         return this._keyToPageSession[key];
     } else if (create) {
-        var pageSession = new Companion.PageSession(this, key);
+        var pageSession = new Companion.PageSession(this, this._dom.pageSessionContainer);
         this._keyToPageSession[key] = pageSession;
         return pageSession;
     } else {
@@ -31,11 +33,15 @@ Companion.WindowSession.prototype.showUserInterface = function() {
         this._uiInitialized = true;
     }
     
+    this._tabHasFocus = true;
+    this._updateUserInterface();
+    
     var crossWindowDeck = document.getElementById("companion-crossWindowDeck");
     crossWindowDeck.selectedPanel = this._windowSessionPage;
 }
 
 Companion.WindowSession.prototype.hideUserInterface = function() {
+    this._tabHasFocus = false;
 }
 
 Companion.WindowSession.prototype._initializeUserInterface = function() {
@@ -48,177 +54,57 @@ Companion.WindowSession.prototype._initializeUserInterface = function() {
     this._windowSessionPage = windowSessionPageTemplate.cloneNode(true);
     this._windowSessionPage.hidden = false;
     
-    /*
-     *  Retrieve references to inner elements.
-     */
-    this._dom.stageDeck = this._windowSessionPage.getElementsByTagName("deck")[0];
-    this._dom.startStage = this._dom.stageDeck.getElementsByTagName("vbox")[0];
-    this._dom.analyzeButton = this._dom.startStage.getElementsByTagName("button")[0];
-    this._dom.typeListbox = this._dom.startStage.getElementsByTagName("listbox")[0];
-    this._dom.propertyListbox = this._dom.startStage.getElementsByTagName("listbox")[1];
+    this._dom.pageSessionContainer = this._windowSessionPage.getElementsByTagName("vbox")[0];
     
-    /*
-     *  Wire up event handlers.
-     */
-    this._dom.analyzeButton.addEventListener('command', function(event) { self._onAnalyzeCommand(); }, true);
-    
-    /*
-     *  Add our panel to the common deck.
-     */
     var crossWindowDeck = document.getElementById("companion-crossWindowDeck");
     crossWindowDeck.appendChild(this._windowSessionPage);
-}
-
-Companion.WindowSession.prototype._onAnalyzeCommand = function() {
-    this._getOpenCalaisAnnotation();
-};
-
-Companion.WindowSession.prototype._getOpenCalaisAnnotation = function() {
-    var self = this;
     
-    var textFragments = [];
+    /*
+     *  Listen for navigation events.
+     */
+    this._progressListener = {
+        QueryInterface : function(iid) {
+            if (iid.equals(Components.interfaces.nsIWebProgressListener) ||
+                iid.equals(Components.interfaces.nsISupportsWeakReference) ||
+                iid.equals(Components.interfaces.nsISupports)) {
+                return this;
+            }
+            throw Components.results.NS_NOINTERFACE;
+        },
     
-    var processNode = function(node) {
-        switch (node.nodeType) {
-        case 3: // text node
-            textFragments.push(node.nodeValue);
-            break;
-        case 1: // element
-            var nodeName = node.nodeName.toLowerCase();
-            switch (nodeName) {
-            case "script":
-            case "input":
-            case "button":
-            case "option":
-            case "select":
-                return; // ignore
+        stateIsRequest: false,
+        onStateChange : function() {},
+        onProgressChange : function() {},
+        onStatusChange : function() {},
+        onSecurityChange : function() {},
+        onLinkIconAvailable : function() {}, 
+        onLocationChange: function(progress, request, location) {
+            // Only watch windows that are their own parent - e.g. not frames
+            if (progress.DOMWindow.parent == progress.DOMWindow) {
+                if (self._tabHasFocus) {
+                    self._updateUserInterface();
+                }
             }
-            var childNode = node.firstChild;
-            while (childNode != null) {
-                processNode(childNode);
-                childNode = childNode.nextSibling;
-            }
-            break; 
         }
     };
-    processNode(this.browser.contentDocument.body);
-    
-    var text = textFragments.join("\n");
-    OpenCalaisUtil.analyzeText(text, function(xmlDoc) {
-        self._onOpenCalaisTextAnalysisResult(xmlDoc);
-    });
+    this.browser.addProgressListener(this._progressListener, 
+        Components.interfaces.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
 };
 
-Companion.WindowSession.prototype._onOpenCalaisTextAnalysisResult = function(xmlDoc) {
-    var self = this;
-    //Companion.inspect(xmlDoc);
-    
-    var root = xmlDoc.firstChild.nextSibling;
-    var entities = root.getElementsByTagName("Entities")[0].childNodes;
-    var list = [];
-    var map = {};
-    for (var i = 0 ; i < entities.length ; i++) {
-        var entityNode = entities[i];
-        var entityType = entityNode.nodeName;
-        
-        var detectionNode = entityNode.getElementsByTagName("Detection")[0];
-        var detection = {
-            text:   detectionNode.firstChild.nodeValue,
-            offset: parseInt(detectionNode.getAttribute("offset")),
-            length: parseInt(detectionNode.getAttribute("length"))
-        };
-        
-        var normalizedName;
-        try {
-            normalizedName = entityNode.getElementsByTagName(entityType)[0].firstChild.nodeValue;
-        } catch (ex) {
-            // Event & Fact (BUG!!!)
-            normalizedName = detection.text;
+Companion.WindowSession.prototype._updateUserInterface = function() {
+    var pageSession = this.getCurrentPageSession(true);
+    if (pageSession != this._currentPageSession) {
+        if (this._currentPageSession != null) {
+            this._currentPageSession.uninstallUserInterface();
         }
+        pageSession.installUserInterface();
         
-        if (normalizedName in map) {
-            map[normalizedName].detections.push(detection);
-        } else {
-            map[normalizedName] = {
-                entityType: entityType,
-                detections: [ detection ]
-            };
-            list.push(normalizedName);
-        }
+        this._currentPageSession = pageSession;
     }
-    
-    var entries = [];
-    for (var i = 0; i < list.length; i++) {
-        var normalizedName = list[i];
-        var entity = map[normalizedName];
-        var entry = {
-            name:           normalizedName,
-            freebaseTypes:  [],
-            detections:     entity.detections
-        };
-        
-        if (entity.entityType in OpenCalaisUtil.entityTypeMap) {
-            entry.freebaseTypes = [].concat(OpenCalaisUtil.entityTypeMap[entity.entityType].freebaseTypes);
-        }
-        
-        entries.push(entry);
-    }
-    
-    FreebaseOracle.reconcile(
-        entries, 
-        function() { self._onDoneReconciliation(entries); }
-    );
 };
 
-Companion.WindowSession.prototype._onDoneReconciliation = function(entries) {
-    var self = this;
-    
-    var ids = [];
-    for (var i = 0; i < entries.length; i++) {
-        var entry = entries[i];
-        var rr = entry.freebaseReconciliationResult;
-        if ("id" in rr) {
-            ids.push(rr.id);
-        } else {
-            Companion.log(entry.name + " = unknown");
-        }
-    }
-    
-    FreebaseOracle.getAllRelationships(
-        ids, 
-        function(results) { self._onDoneGetAllRelationships(ids, results); }
-    );
+Companion.WindowSession.prototype._onAnalyzeCommand = function() {
+    var pageSession = this.getCurrentPageSession(true);
+    pageSession.onAnalyzeCommand();
 };
 
-Companion.WindowSession.prototype._onDoneGetAllRelationships = function(ids, results) {
-    Companion.log("Got " + results.length + " relationships");
-    
-    var typeListbox = this._dom.typeListbox;
-    var propertyListbox = this._dom.propertyListbox;
-    while (typeListbox.getRowCount() > 0) {
-        typeListbox.removeItemAt(0);
-    }
-    while (propertyListbox.getRowCount() > 0) {
-        propertyListbox.removeItemAt(0);
-    }
-    
-    var properties = {};
-    for (var i = 0; i < results.length; i++) {
-        var r = results[i];
-        properties[r.master_property] = true;
-    }
-    for (var n in properties) {
-        propertyListbox.appendItem(n, n);
-    }
-    
-    var database = Companion.Database.create();
-    database.loadFreebaseItems(results);
-    
-    var items = new Companion.Set(ids);
-    var types = database.getObjectsUnion(items, "type");
-    types.visit(function(t) {
-        var count = database.countDistinctSubjects(t, "type", items);
-        var text = t + " (" + count + ")";
-        typeListbox.appendItem(text, text);
-    });
-};
