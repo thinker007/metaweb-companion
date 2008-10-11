ed6a6446f279f0d0f05e2a6346f338e169501030
dfhuynh <dfhuynh@36899aa3-f54f-0410-b718-5f787d48e281>
2008-10-11 00:29:01 +0000

    Started switching over to metaweb-suite for faceted browsing.

--- src/extension/content/scripts/overlay.js
+++ src/extension/content/scripts/overlay.js
@@ -1,5 +1,4 @@
 var Companion = {
-    sidepaneHidden:  true,
     _consoleService: Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService)
 };
 
@@ -17,12 +16,12 @@ Companion.inspect = function(o) {
 
 Companion.getHelperURL = function() {
     var url = "http://localhost:8192/";
-	var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-	try {
-		url = prefs.getCharPref("metaweb.companion.helper");
-	} catch (e) {
-	}
-	return url;
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+    try {
+        url = prefs.getCharPref("metaweb.companion.helper");
+    } catch (e) {
+    }
+    return url;
 };
 
 Companion.cancelEvent = function(evt) {
@@ -52,21 +51,25 @@ Companion._onSelectTab = function(event) {
     var selectedIndex = tabBrowser.selectedTab.parentNode.selectedIndex;
     
     var browsers = tabBrowser.browsers;
+    var selectedWindowSession = null;
     for (var i = 0; i < browsers.length; i++) {
         var browser = tabBrowser.browsers[i];
         if (i == selectedIndex) {
             var companionWindowSession;
-	        if ("_companionWindowSession" in browser) {
-	            companionWindowSession = browser._companionWindowSession;
-	        } else { 
-	            companionWindowSession = new Companion.WindowSession(browser);
+            if ("_companionWindowSession" in browser) {
+                companionWindowSession = browser._companionWindowSession;
+            } else { 
+                companionWindowSession = new Companion.WindowSession(browser);
                 browser._companionWindowSession = companionWindowSession;
             }
-            
-            companionWindowSession.showUserInterface();
+            selectedWindowSession = companionWindowSession;
         } else if ("_companionWindowSession" in browser) {
-            browser._companionWindowSession.hideUserInterface();
+            browser._companionWindowSession.onLostFocus();
         }
+    }
+    
+    if (selectedWindowSession != null) {
+        selectedWindowSession.onGotFocus();
     }
 };
 
@@ -75,21 +78,42 @@ Companion._onCloseTab = function(event) {
     Companion.inspect(event);
 };
 
-Companion.toggleSidepane = function() {
-	Companion.sidepaneHidden = !Companion.sidepaneHidden;
-	
-	document.getElementById("companion-sidePaneSplitter").hidden = Companion.sidepaneHidden;
-	document.getElementById("companion-sidePane").hidden = Companion.sidepaneHidden;
-	document.getElementById("companion-toggleSidepane-cmd").
-		setAttribute("checked", !Companion.sidepaneHidden);
+Companion.activate = function() {
+    Companion._getCurrentWindowSession().activate();
+};
+
+Companion._getCurrentWindowSession = function() {
+    return Companion._getSelectedBrowser()._companionWindowSession;
+};
+
+Companion._getSelectedBrowser = function() {
+    var tabBrowser = document.getElementById("content");
+    var selectedIndex = tabBrowser.selectedTab.parentNode.selectedIndex;
+    var browsers = tabBrowser.browsers;
+    return browsers[selectedIndex];
 };
 
 Companion.isDatawebDocument = function(doc) {
-	return doc.location.href.indexOf("dataweb:") == 0 ||
-		doc.location.href.indexOf("chrome://companion/content/dataweb-commands/") == 0;
+    return doc.location.href.indexOf("dataweb:") == 0 ||
+        doc.location.href.indexOf("chrome://companion/content/dataweb-commands/") == 0;
 };
 
 Companion.isMultiwebDocument = function(doc) {
-	return doc.location.href.indexOf("multiweb:") == 0 ||
-		doc.location.href.indexOf("chrome://companion/content/multiweb-commands/") == 0;
+    return doc.location.href.indexOf("multiweb:") == 0 ||
+        doc.location.href.indexOf("chrome://companion/content/multiweb-commands/") == 0;
+};
+
+Companion.openSidePane = function() {
+    document.getElementById("companion-sidePaneSplitter").hidden = false;
+    document.getElementById("companion-sidePane").hidden = false;
+};
+
+Companion.closeSidebar = function() {
+    document.getElementById("companion-sidePaneSplitter").hidden = true;
+    document.getElementById("companion-sidePane").hidden = true;
+};
+
+Companion.addSidePaneContent = function(elmt) {
+    var sidePane = document.getElementById("companion-sidePane");
+    sidePane.insertBefore(elmt, sidePane.lastChild);
 };
--- src/extension/content/scripts/ui/highlighters/highlighter-util.js
+++ src/extension/content/scripts/ui/highlighters/highlighter-util.js
@@ -1,10 +1,10 @@
 Companion.HighlighterUtil = {
-	augmentingStyles: {
-		styleID: 		"metawebCompanion-style",
-		flagID:			"metawebCompanion-style-flag", // we need this because the browser seems to keep the style element around
-		detectionClass: "metawebCompanion-detection",
-		highlightClass: "metawebCompanion-highlight"
-	}
+    augmentingStyles: {
+        styleID:         "metawebCompanion-style",
+        flagID:            "metawebCompanion-style-flag", // we need this because the browser seems to keep the style element around
+        detectionClass: "metawebCompanion-detection",
+        highlightClass: "metawebCompanion-highlight"
+    }
 };
 
 Companion.HighlighterUtil.addAugmentingStyles = function(doc) {
@@ -25,23 +25,23 @@ Companion.HighlighterUtil.addAugmentingStyles = function(doc) {
         
         head.appendChild(style);
     }
-	
+    
     var flag = doc.getElementById(Companion.HighlighterUtil.augmentingStyles.flagID);
     if (!flag) {
-		flag = doc.createElement("div");
-		flag.id = Companion.HighlighterUtil.augmentingStyles.flagID;
-		doc.body.appendChild(flag);
-	}
+        flag = doc.createElement("div");
+        flag.id = Companion.HighlighterUtil.augmentingStyles.flagID;
+        doc.body.appendChild(flag);
+    }
 };
 
 Companion.HighlighterUtil.removeAugmentingStyles = function(doc) {
     var style = doc.getElementById(Companion.HighlighterUtil.augmentingStyles.styleID);
     if (style) {
-		style.parentNode.removeChild(style);
+        style.parentNode.removeChild(style);
     }
     var flag = doc.getElementById(Companion.HighlighterUtil.augmentingStyles.flagID);
     if (flag) {
-		flag.parentNode.removeChild(style);
+        flag.parentNode.removeChild(style);
     }
 };
 
@@ -50,139 +50,144 @@ Companion.HighlighterUtil.hasAugmentingStyles = function(doc) {
 };
 
 Companion.HighlighterUtil.addAugmentations = function(doc, identityModel, onclick) {
-	var ignore = { 
-		"i":true, "you":true, "he":true, "she":true, "it":true, "we":true, "they":true, 
-		"me":true, "him":true, "her":true, "us":true, "them":true,
-		"his":true, "hers":true, "mine":true, "yours":true, "ours":true, "theirs":true
-	};
-	
-	var index = identityModel.createManifestationIndex(ignore);
-	
-	var processElement = function(elmt) {
-		var tagName = elmt.tagName;
-		if (tagName == "script") {
-			return;
-		}
-		
-		var child = elmt.firstChild;
-		while (child != null) {
-			// processNode might change the child (such as removing it) so we need to save its next sibling first
-			var nextChild = child.nextSibling;
-			
-			processNode(child);
-			
-			child = nextChild;
-		}
-	};
-	
-	var manifestations = index.manifestations;
-	var processTextNode = function(textNode) {
-		var text = textNode.nodeValue;
-		
-		var i = 0;
-		for (; i < manifestations.length; i++) {
-			var manifestation = manifestations[i];
-			if (manifestation.length <= text.length) {
-				break;
-			}
-		}
-		for (; i < manifestations.length; i++) {
-			var manifestation = manifestations[i];
-			var l = text.indexOf(manifestation);
-			if (l >= 0) {
-				var before = text.substr(0, l);
-				var after = text.substr(l + manifestation.length);
-				var parentNode = textNode.parentNode;
-				
-				var span = doc.createElement("span");
-				span.className = Companion.HighlighterUtil.augmentingStyles.detectionClass;
-				span.appendChild(doc.createTextNode(manifestation));
-				parentNode.insertBefore(span, textNode);
-				
-				var ids = [];
-				for (var id in index.manifestationToIDs[manifestation]) {
-					ids.push(id);
-				}
-				span.setAttribute("itemIDs", ids.join(";"));
+    var ignore = { 
+        "i":true, "you":true, "he":true, "she":true, "it":true, "we":true, "they":true, 
+        "me":true, "him":true, "her":true, "us":true, "them":true,
+        "his":true, "hers":true, "mine":true, "yours":true, "ours":true, "theirs":true
+    };
+    
+    var index = identityModel.createManifestationIndex(ignore);
+    
+    var processElement = function(elmt) {
+        var tagName = elmt.tagName;
+        if (tagName == "script") {
+            return;
+        }
+        
+        var child = elmt.firstChild;
+        while (child != null) {
+            // processNode might change the child (such as removing it) so we need to save its next sibling first
+            var nextChild = child.nextSibling;
+            
+            processNode(child);
+            
+            child = nextChild;
+        }
+    };
+    
+    var manifestations = index.manifestations;
+    var processTextNode = function(textNode) {
+        var text = textNode.nodeValue;
+        
+        var i = 0;
+        for (; i < manifestations.length; i++) {
+            var manifestation = manifestations[i];
+            if (manifestation.length <= text.length) {
+                break;
+            }
+        }
+        for (; i < manifestations.length; i++) {
+            var manifestation = manifestations[i];
+            var l = text.indexOf(manifestation);
+            if (l >= 0) {
+                var before = text.substr(0, l);
+                var after = text.substr(l + manifestation.length);
+                var parentNode = textNode.parentNode;
+                
+                var span = doc.createElement("span");
+                span.className = Companion.HighlighterUtil.augmentingStyles.detectionClass;
+                span.appendChild(doc.createTextNode(manifestation));
+                parentNode.insertBefore(span, textNode);
+                
+                var ids = [];
+                for (var id in index.manifestationToIDs[manifestation]) {
+                    ids.push(id);
+                }
+                span.setAttribute("itemIDs", ids.join(";"));
                 span.addEventListener('click', function(evt) {
-					return Companion.HighlighterUtil._onClickAugmentation(evt, this, onclick); 
-				}, true);
-				
-				if (before.length > 0) {
-					var beforeTextNode = doc.createTextNode(before);
-					parentNode.insertBefore(beforeTextNode, span);
-					processTextNode(beforeTextNode);
-				}
-				if (after.length > 0) {
-					textNode.nodeValue = after;
-					processTextNode(textNode);
-				} else {
-					parentNode.removeChild(textNode);
-				}
-				return;
-			}
-		}
-	};
-	
-	var processNode = function(node) {
-		if (node != null) {
-			switch (node.nodeType) {
-			case 1:
-				processElement(node);
-				break;
-			case 3:
-				processTextNode(node);
-				break;
-			}
-		}
-	};
-	
-	processNode(doc.body);
+                    return Companion.HighlighterUtil._onClickAugmentation(evt, this, onclick); 
+                }, true);
+                
+                if (before.length > 0) {
+                    var beforeTextNode = doc.createTextNode(before);
+                    parentNode.insertBefore(beforeTextNode, span);
+                    processTextNode(beforeTextNode);
+                }
+                if (after.length > 0) {
+                    textNode.nodeValue = after;
+                    processTextNode(textNode);
+                } else {
+                    parentNode.removeChild(textNode);
+                }
+                return;
+            }
+        }
+    };
+    
+    var processNode = function(node) {
+        if (node != null) {
+            switch (node.nodeType) {
+            case 1:
+                processElement(node);
+                break;
+            case 3:
+                processTextNode(node);
+                break;
+            }
+        }
+    };
+    
+    processNode(doc.body);
 };
 
 Companion.HighlighterUtil.removeAugmentations = function(doc) {
-	// TODO
+    // TODO
 };
 
 Companion.HighlighterUtil.highlightAugmentations = function(doc, identities, animationCallback) {
-	var spans = doc.getElementsByTagName("span");
-	var spansToHighlight = [];
-	
-	for (var i = 0; i < spans.length; i++) {
-		var span = spans[i];
-		var classes = span.className;
-		if (classes.indexOf(Companion.HighlighterUtil.augmentingStyles.detectionClass) >= 0) {
-			var found = false;
-			var itemIDs = span.getAttribute("itemIDs");			
-			itemIDs = itemIDs.split(";");
-			
-			for (var j = 0; j < itemIDs.length; j++) {
-				if (identities.contains(itemIDs[j])) {
-					found = true;
-					break;
-				}
-			}
-			
-			if (found) {
-				span.className = Companion.HighlighterUtil.augmentingStyles.detectionClass + " " + Companion.HighlighterUtil.augmentingStyles.highlightClass;
-				spansToHighlight.push(span);
-			} else {
-				span.className = Companion.HighlighterUtil.augmentingStyles.detectionClass;
-			}
-		}
-	}
-	
-	animationCallback(spansToHighlight);
+    var spans = doc.getElementsByTagName("span");
+    var spansToHighlight = [];
+    
+    var map = {};
+    for (var i = 0; i < identities.length; i++) {
+        map[identities[i]] = true;
+    }
+    
+    for (var i = 0; i < spans.length; i++) {
+        var span = spans[i];
+        var classes = span.className;
+        if (classes.indexOf(Companion.HighlighterUtil.augmentingStyles.detectionClass) >= 0) {
+            var found = false;
+            var itemIDs = span.getAttribute("itemIDs");            
+            itemIDs = itemIDs.split(";");
+            
+            for (var j = 0; j < itemIDs.length; j++) {
+                if (itemIDs[j] in map) {
+                    found = true;
+                    break;
+                }
+            }
+            
+            if (found) {
+                span.className = Companion.HighlighterUtil.augmentingStyles.detectionClass + " " + Companion.HighlighterUtil.augmentingStyles.highlightClass;
+                spansToHighlight.push(span);
+            } else {
+                span.className = Companion.HighlighterUtil.augmentingStyles.detectionClass;
+            }
+        }
+    }
+    
+    animationCallback(spansToHighlight);
 };
 
 Companion.HighlighterUtil._onClickAugmentation = function(evt, elmt, onclick) {
     if (elmt.className.indexOf(Companion.HighlighterUtil.augmentingStyles.highlightClass) >= 0) {
         var itemIDs = elmt.getAttribute("itemIDs");
         try {
-			onclick(itemIDs.split(";"));
-		} catch (e) {
-			Companion.log(e);
-		}
+            onclick(itemIDs.split(";"));
+        } catch (e) {
+            Companion.log(e);
+        }
             
         return Companion.cancelEvent(evt);
     }
